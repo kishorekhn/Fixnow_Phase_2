a3ab08ac222a67695eac14ff198a39137fa6e711
sanloop-akshay <akshaypranavb@gmail.com>
2025-06-15 17:27:06 +0530

    Views Init

--- APP/views.py
+++ APP/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+#Main Templates Backend
 def home(request):
     return render(request,"Main/home.htm")
+
+
+#Auth Templates Backend
+def signin(request):
+    return render(request,"Auth/signin.htm")
+
+
+#Admin Templates Backend
+def addServices(request):
+    pass
