a5ff535abe1a50e4f20d7622bccb491f809a3e40
sanloop-akshay <akshaypranavb@gmail.com>
2025-07-26 12:47:41 +0530

    Changed DB Credentials

--- FixNow/settings.py
+++ FixNow/settings.py
@@ -56,11 +56,17 @@ WSGI_APPLICATION = 'FixNow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.bgdkjvlojbxkbnqqbnze',
+        'PASSWORD': 'db9cXxjufMMhqdma',
+        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+        'PORT': '6543',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
