916cddcaece96c6df9ab8afcf5bb88ae955c231e
sanloop-akshay <akshaypranavb@gmail.com>
2025-07-27 17:30:00 +0530

    Frontend - dummy design intiated

--- FixNow/urls.py
+++ FixNow/urls.py
@@ -8,15 +8,16 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('',views.home,name='home'),
     path('services/<int:service_id>/', views.service_items, name='service_items'),
     path('verify-otp/', views.verify_otp_view, name='verify_otp'),
     path('toggle-cart/', views.toggle_cart, name='toggle_cart'), 
     path('toggle-favorite/', views.toggle_favorite, name='toggle_favorite'),
     path('orders/', views.orders_view, name='orders'),
     path('favorites/', views.favorites_view, name='favorites'),
+    path('contact',views.contact_view, name='contact'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
